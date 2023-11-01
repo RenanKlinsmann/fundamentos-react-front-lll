7a62e5e842c036e24b145b851075c6883e8891ef
Renan Mendonça <klismann-nan@hotmail.com>
2023-11-01 20:34:38 -0300

    Exercicio produtos

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,29 @@ import Fragmento from './componentes/basicos/Fragmento';
 import Pai from './componentes/Familia/Pai';
 import Padrasto from './componentes/Familia/Padrasto';
 import Filho from './componentes/Familia/Filho';
+import PaiExercicio from './componentes/exercicio#01/PaiExercicio';
+import DiretaPai from './componentes/comunicao/DiretaPai';
+import IndiretaPai from './componentes/comunicao/IndiretaPai';
+import ListAlunos from './componentes/repeticao/ListAlunos';
 
 export default () => {
 
     return (
         <div className="App">
             <h1>Fundamentos React</h1>
-            <div className="Cards">
+            <div className="Cards">,
+                <Card titulo="#09 - Repetição" color='#008B8B'>
+                   <ListAlunos/>
+                </Card>
+                <Card titulo="#08 - Comunicação Indireta" color='#FF7F50'>
+                   <IndiretaPai/>
+                </Card>
+                <Card titulo="#07 - Comunicação Direta" color='#7FFF00'>
+                   <DiretaPai/>
+                </Card>
+                <Card titulo="#06 - Exercicio #02" color='#7FFFD4'>
+                   <PaiExercicio/>
+                </Card>
                 <Card titulo="#05 - Padastro e Filho" color='#006400'>
                     <Padrasto sobrenome="Uniesp">
                         <Filho nome="Teste Padrastro 1"/>
